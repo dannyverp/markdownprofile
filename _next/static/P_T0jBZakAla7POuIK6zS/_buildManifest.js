efaaa2d02f340ea934d7e876a20f5d428c9ac743
dannyverp <dannyverp@users.noreply.github.com>
2020-07-14 21:55:39 +0000

    deploy: b8e88af1d45721febaaae3d38dd8cc19efeabbc5

---
+++ _next/static/P_T0jBZakAla7POuIK6zS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002Fcfb35ab8.b6aecf4995e485f6a054.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
