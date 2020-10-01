713925d9821df8aaff6f0a83c6e705c3e2cd52ee
AnkitJuyal009 <ankitjuyal2000@gmail.com>
2020-10-01 08:57:40 +0530

    Enhances the template card component

--- components/templateSelector.js
+++ components/templateSelector.js
@@ -1,43 +1,43 @@
 class TemplateSelector extends React.Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        const className = "flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800" +
-            (this.props.activeTemplate === this.props.template ? " bg-orange-400" : '');
-        return (
-            <div className="flex flex-col items-center justify-center bg-white p-4 shadow rounded-lg">
-                <div
-                    className="inline-flex overflow-hidden">
-                    <img
-                        src={this.props.image}
-                        alt=""
-                        className="h-full w-full"/>
-                </div>
+  render() {
+    const className =
+      'flex items-center justify-center h-8 w-8 border rounded-full text-gray-800 border-gray-800' +
+      (this.props.activeTemplate === this.props.template
+        ? ' bg-orange-400'
+        : '');
+    return (
+      <div
+        onClick={() => {
+          if (typeof this.props.onTemplateChange === 'function') {
+            this.props.onTemplateChange(this.props.template);
+          }
+        }}
+        className="flex flex-col items-center justify-center bg-white p-4 shadow rounded-lg hover:shadow-xl"
+      >
+        <div className="inline-flex overflow-hidden">
+          <img src={this.props.image} alt="" className="h-full w-full" />
+        </div>
 
-                <h2 className="mt-4 font-bold text-xl">{this.props.title}</h2>
-                <h6 className="mt-2 text-sm font-medium">{this.props.author}</h6>
+        <h2 className="mt-4 font-bold text-xl">{this.props.title}</h2>
+        <h6 className="mt-2 text-sm font-medium">{this.props.author}</h6>
+        <p className="text text-gray-500 text-center mt-3">
+          {this.props.description}
+        </p>
 
-                <p className="text text-gray-500 text-center mt-3">
-                    {this.props.description}
-                </p>
-
-                <ul className="flex flex-row mt-4 space-x-2">
-                    <li>
-                        <a onClick={() => {
-                            if (typeof this.props.onTemplateChange === 'function') {
-                                this.props.onTemplateChange(this.props.template);
-                            }
-                        }}
-                           className={className}>
-                            <i className="fab fa-facebook"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        )
-    }
+        <ul className="flex flex-row mt-4 space-x-2">
+          <li>
+            <a className={className}>
+              <i className="fab fa-facebook"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default TemplateSelector
+export default TemplateSelector;
