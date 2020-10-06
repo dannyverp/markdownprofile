af7f614b98d9736bd0bcb392a8d9491108b9a0ca
dannyverp <dannyverp@users.noreply.github.com>
2020-10-06 14:44:50 +0000

    deploy: 334b9da33ed1256b74c5ffcf4acede69fe1a5d49

---
+++ _next/static/yQAs2pU6UFGOxOHU5tsoG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fcfb35ab8.9986397e2f36de5447b9.js","static\u002Fchunks\u002Fpages\u002Findex-39088d9bf19224113716.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-cb301e4e9e10851ffa77.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
